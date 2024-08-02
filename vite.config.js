a3b243cffbf3f82b81febc232ad99f96098ce2bd
miecrose12 <itobiyeblossom75@gmail.com>
2024-08-02 10:58:47 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+/** @format */
+ 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+ 
+ 
 export default defineConfig({
+  envPrefix: "REACT_APP_",
+  // @ts-ignore
   plugins: [react()],
-})
+  server: {
+    open: true,
+    port: 3007,
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
+  build: {
+    outDir: "build",
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
+ 
